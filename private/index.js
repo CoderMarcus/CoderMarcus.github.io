707ce2928a26e7e2435170de4c4a379965d25d57
Marcus Jones <46655509+Marcus5408@users.noreply.github.com>
2021-12-03 22:59:20 -0500

    ewsdfgaweg

--- private/index.js
+++ private/index.js
@@ -1,1 +1,16 @@
-//create a centered div
+var div = document.createElement('div');
+div.innerHTML = '<h1>Hello World</h1>';
+document.body.appendChild(div);
+
+div.style.position = 'absolute';
+div.style.top = '50%';
+div.style.left = '50%';
+div.style.transform = 'translate(-50%, -50%)';
+
+div.querySelector('input').addEventListener('inout', function(e) {
+    var value = e.detail.value
+    if (value.match(/^#[0-9a-f]{6}$/i)) {
+        document.body.style.backgroundColor = value
+    }else{
+        document.body.style.backgroundColor = '#fff'
+    }
