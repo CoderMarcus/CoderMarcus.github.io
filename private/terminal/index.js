f68ec0bb3b38901ce71e3f4cb6664eb8243047ab
Marcus Jones <46655509+Marcus5408@users.noreply.github.com>
2022-05-04 21:53:55 -0400

    yes

--- private/terminal/index.js
+++
@@ -1,8 +0,0 @@
-const emulator = new Terminal.Emulator();
-const emulatorState = Terminal.EmulatorState.createEmpty();
-
-const commandStr = 'ls';
-const plugins = [];
-
-const newEmulatorState = emulator.execute(emulatorState, commandStr, plugins)
-newEmulatorState.getOutputs()
